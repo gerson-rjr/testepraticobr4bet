98ba49f38aac7836703b9d09d6b7c37d9b4fd4ef
Gerson Junior <gersonjunior2@outlook.com>
2023-09-04 17:48:17 -0300

    adicionando migration, model e controller resource

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+
+    public function index()
+    {
+        
+    }
+
+
+    public function create()
+    {
+        
+    }
+
+
+    public function store(Request $request)
+    {
+      
+    }
+
+
+    public function show(Usuario $usuario)
+    {
+       
+    }
+
+
+    public function edit(Usuario $usuario)
+    {
+       
+    }
+
+
+    public function update(Request $request, Usuario $usuario)
+    {
+       
+    }
+
+
+    public function destroy(Usuario $usuario)
+    {
+      
+    }
+}
